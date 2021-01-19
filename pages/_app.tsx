8b887d4345a4aac341289b241adf3229e812a99b
piazzatron <michael.piazza.mp@gmail.com>
2021-01-19 00:19:12 -0800

    Default SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from 'next/app'
 import NavImageContextProvider from '../components/nav-image-context/NavImageContext'
 import Head from 'next/head'
 import StateProvider from '../components/state-provider/StateProvider'
+import { DefaultSeo } from 'next-seo'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,16 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="user-scalable=no, width=device-width, initial-scale=1.0"
         />
       </Head>
+      <DefaultSeo
+        title="Michael Piazza Photography"
+        description="Michael Piazza's work and photojournal. Shot mostly on Fuji X."
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: 'https://www.piazza.photos/',
+          site_name: 'Michael Piazza Photography',
+        }}
+      />
       <NavImageContextProvider>
         <StateProvider>
           <Component {...pageProps} />
